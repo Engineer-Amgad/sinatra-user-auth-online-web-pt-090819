b1113a9a2be5bfd0135ac353559928ea16d4317d
Amgad Malik <amgadmalik@yahoo.com>
2019-12-31 17:39:47 +0000

    Automatically backed up by Learn

--- db/migrate/20191231173534_create_users.rb
+++ db/migrate/20191231173534_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :unsers do |t|
       t.string :name 
+      t.string :email 
+      t.string :password 
       
     end 
   end
